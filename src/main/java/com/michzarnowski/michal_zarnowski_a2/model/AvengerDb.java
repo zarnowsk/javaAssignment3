ac219da93f0b9812a7d2aa4677e945edda7df0e8
Michal Zarnowski <zarnowsk@sheridancollege.ca>
2019-11-13 09:46:27 -0500

    Connection is now acquired through another class(DBConnector.java)

--- src/main/java/com/michzarnowski/michal_zarnowski_a2/model/AvengerDb.java
+++ src/main/java/com/michzarnowski/michal_zarnowski_a2/model/AvengerDb.java
@@ -1,7 +1,7 @@
 package com.michzarnowski.michal_zarnowski_a2.model;
 
+import com.michzarnowski.michal_zarnowski_a2.db.DBConnector;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,61 +14,72 @@ public class AvengerDb {
     }
 
     public ArrayList<Avenger> getAvengers() {
-        
+
+        //List of Avengers to be returned
         ArrayList<Avenger> avengerList = new ArrayList<>();
+        
+        //DB specific variables
+        String connUrl = "jdbc:postgresql://localhost/";
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        String database = "AvengerDb";
+        String user = "postgres";
+        String pass = "a1b2c3d3";
+
+        //postgreSQL driver
+        String driver = "org.postgresql.Driver";
 
         try {
-            //postgreSQL driver
-            String driver = "org.postgresql.Driver";
+            // This enviroment variable is how we get the database info on Heroku
+            // it is populated by Heroku with what we need to connect to the provisioned database
+            // It will be null on our local machines or if no db add-on
+            String dbUrl = System.getenv("JDBC_DATABASE_URL");
 
-            //Load the driver
-            Class.forName(driver).newInstance();
+            if (dbUrl != null && dbUrl.length() > 0) {
+                conn = DBConnector.getConnection(driver, dbUrl);
+            } else {
+                //Load the driver
+                Class.forName(driver).newInstance();
 
-            //Declare and initialize connection
-            Connection conn = null;
+                //Database connection
+                conn = DBConnector.getConnection(driver, connUrl, database,
+                        user, pass);
 
-            //Specify DB
-            String connUrl = "jdbc:postgresql://localhost/AvengerDb";
+                //Declare a query
+                String sqlQuery = "SELECT * FROM avengers";
 
-            //User and password
-            String user = "postgres";
-            String pass = "a1b2c3d3";
+                //Create and set the statement
+                stmt = conn.createStatement();
+                rs = stmt.executeQuery(sqlQuery);
 
-            //Connect!
-            conn = DriverManager.getConnection(connUrl, user, pass);
+                //Loop through results
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    String name = rs.getString("avengername");
+                    String description = rs.getString("description");
+                    int powerSourceInt = rs.getInt("powersource");
 
-            //Declare a query
-            String sqlQuery = "SELECT * FROM avengers";
+                    //Create power source object via PowerSourceDb
+                    PowerSourceDb powerSourceDb = new PowerSourceDb();
+                    PowerSource tempPowerSource
+                            = powerSourceDb.getPowerSource(powerSourceInt);
 
-            //Create and set the statement
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sqlQuery);
+                    //create Avenger objects based on DB data
+                    Avenger tempAvenger = new Avenger(id, name, description,
+                            tempPowerSource);
 
-            //Loop through results
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("avengername");
-                String description = rs.getString("description");
-                int powerSourceInt = rs.getInt("powersource");
-                
-                //Create power source object via PowerSourceDb
-                PowerSourceDb powerSourceDb = new PowerSourceDb();
-                PowerSource tempPowerSource = 
-                        powerSourceDb.getPowerSource(powerSourceInt);
-                
-                //create Avenger objects based on DB data
-                Avenger tempAvenger = new Avenger(id, name, description, 
-                        tempPowerSource);
-                
-                //add Avenger to ArrayList
-                avengerList.add(tempAvenger);
+                    //add Avenger to ArrayList
+                    avengerList.add(tempAvenger);
+                }
             }
 
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+            System.out.println(ex);
+        } catch (Exception ex) {
             System.out.println(ex);
         }
 
         return avengerList;
     }
-
 }
