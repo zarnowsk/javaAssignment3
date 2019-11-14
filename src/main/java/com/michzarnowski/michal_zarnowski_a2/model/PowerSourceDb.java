bcad5980524194a92241e329c4a15d97bf5f614d
Michal Zarnowski <zarnowsk@sheridancollege.ca>
2019-11-13 21:11:23 -0500

    Made class level static variables used in all methods. Restructured getPowerSource method to work in cloud environment and use DBConnector class to establish DB connection.

--- src/main/java/com/michzarnowski/michal_zarnowski_a2/model/PowerSourceDb.java
+++ src/main/java/com/michzarnowski/michal_zarnowski_a2/model/PowerSourceDb.java
@@ -2,80 +2,29 @@ package com.michzarnowski.michal_zarnowski_a2.model;
 
 import com.michzarnowski.michal_zarnowski_a2.db.DBConnector;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 public class PowerSourceDb {
 
+    //postgreSQL driver
+    private static String driver = "org.postgresql.Driver";
+    //connection URL
+    private static String connUrl = "jdbc:postgresql://localhost/";
+    //database name
+    private static String database = "AvengerDb";
+    //database credentials
+    private static String user = "postgres";
+    private static String pass = "a1b2c3d3";
+
     public static PowerSource getPowerSource(int id) {
-        
+
         PowerSource powerSource = null;
-
-        try {
-            //postgreSQL driver
-            String driver = "org.postgresql.Driver";
-            
-            //Load the driver
-            Class.forName(driver).newInstance();
-            
-            //Declare and initialize connection
-            Connection conn = null;
-            
-            //Specify DB
-            String connUrl = "jdbc:postgresql://localhost/AvengerDb";
-            
-            //User and password
-            String user = "postgres";
-            String pass = "a1b2c3d3";
-            
-            //Connect!
-            conn = DriverManager.getConnection(connUrl, user, pass);
-            
-            //Acquire corresponding power source from DB
-            //Power source query
-            String powerQuery = "SELECT * FROM powersource WHERE id = ?";
-            PreparedStatement ps = conn.prepareStatement(powerQuery);
-            ps.setInt(1, id); //set query param values
-            
-            //Acquire results
-            ResultSet tempRs = ps.executeQuery();
-            
-            //Get result
-            String powerDescription = "";
-            if (tempRs.next()) {
-                powerDescription = tempRs.getString("description");
-            }
-            
-            powerSource = new PowerSource(id, powerDescription);
-            
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
-            System.out.println(ex);
-        }
-        
-        return powerSource;
-    }
-
-    public static ArrayList<PowerSource> getPowerSources() {
-        
-        //List of PowerSources to be returned
-        ArrayList<PowerSource> powerSourcesList = new ArrayList<>();
-        
-        //DB specific variables
-        String connUrl = "jdbc:postgresql://localhost/";
         Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-        String database = "AvengerDb";
-        String user = "postgres";
-        String pass = "a1b2c3d3";
+        ResultSet tempRs = null;
 
-        //postgreSQL driver
-        String driver = "org.postgresql.Driver";
-        
         try {
             // This enviroment variable is how we get the database info on Heroku
             // it is populated by Heroku with what we need to connect to the provisioned database
@@ -92,36 +41,91 @@ public class PowerSourceDb {
                 //Database connection
                 conn = DBConnector.getConnection(driver, connUrl, database,
                         user, pass);
-                
+
+                //Acquire corresponding power source from DB
+                //Power source query
+                String powerQuery = "SELECT * FROM powersource WHERE id = ?";
+                PreparedStatement ps = conn.prepareStatement(powerQuery);
+                ps.setInt(1, id); //set query param values
+
+                //Acquire results
+                tempRs = ps.executeQuery();
+
+                //Get result
+                String powerDescription = "";
+                if (tempRs.next()) {
+                    powerDescription = tempRs.getString("description");
+                }
+
+                powerSource = new PowerSource(id, powerDescription);
+            }
+            //close DB connection 
+            DBConnector.closeJDBCObjects(conn, tempRs);
+
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+            System.out.println(ex);
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        return powerSource;
+    }
+
+    public static ArrayList<PowerSource> getPowerSources() {
+
+        //List of PowerSources to be returned
+        ArrayList<PowerSource> powerSourcesList = new ArrayList<>();
+
+        //DB specific variables
+        Connection conn = null;
+        ResultSet rs = null;
+
+        try {
+            // This enviroment variable is how we get the database info on Heroku
+            // it is populated by Heroku with what we need to connect to the provisioned database
+            // It will be null on our local machines or if no db add-on
+            // Info copyrights: PAUL BONENFANT
+            String dbUrl = System.getenv("JDBC_DATABASE_URL");
+
+            if (dbUrl != null && dbUrl.length() > 0) {
+                conn = DBConnector.getConnection(driver, dbUrl);
+            } else {
+                //Load the driver
+                Class.forName(driver);
+
+                //Database connection
+                conn = DBConnector.getConnection(driver, connUrl, database,
+                        user, pass);
+
                 //Declare a query
                 String sqlQuery = "SELECT * FROM powersource";
                 PreparedStatement ps = conn.prepareStatement(sqlQuery);
 
                 //Create and set the statement
                 rs = ps.executeQuery();
-                
+
                 //Loop through results
                 while (rs.next()) {
                     int id = rs.getInt("id");
                     String desc = rs.getString("description");
-                    
+
                     //Create power source object 
                     PowerSource tempPowerSource = getPowerSource(id);
-                    
+
                     //add to ArrayList
                     powerSourcesList.add(tempPowerSource);
                 }
             }
-            
+
             //close DB connection 
-            DBConnector.closeJDBCObjects(conn, stmt, rs);
-            
+            DBConnector.closeJDBCObjects(conn, rs);
+
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
             System.out.println(ex);
         } catch (Exception ex) {
             System.out.println(ex);
         }
-                
+
         return powerSourcesList;
     }
 
